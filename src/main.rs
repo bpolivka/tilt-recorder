864b2951e348517b15d0921d58a5fc2abd41f487
Brett Polivka <brett@bolonium.org>
2022-07-05 12:40:28 -0500

    update to scan all availalbe bluetooth ifaces

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-use btleplug::{
-    api::{Central, CentralEvent, Peripheral},
-    bluez::manager::Manager,
-};
+use btleplug::api::{Central, CentralEvent, Manager as _, ScanFilter};
+use btleplug::platform::Manager;
 use chrono::{DateTime, Utc};
+use futures::stream::{select_all, StreamExt};
 use influxdb::{Client, InfluxDbWriteable};
 use std::env;
+use std::error::Error;
 
-const TILT_MFG_ID: [u8; 6] = [0x4c, 0x00, 0x02, 0x15, 0xa4, 0x95];
+const TILT_MFG_ID: u16 = 0x4c;
 
 #[derive(Debug)]
 enum TiltColor {
@@ -50,62 +50,104 @@ fn color_name(color: TiltColor) -> &'static str {
 #[derive(InfluxDbWriteable, Debug)]
 struct TiltReading {
     time: DateTime<Utc>,
-    #[tag]
+    #[influxdb(tag)]
     color: &'static str,
     temp: u16,
     sg: f32,
 }
 
-#[async_std::main]
-async fn main() {
-    let adapter_name = env::var("BT_ADAPTER").unwrap();
-
-    let manager = Manager::new().unwrap();
-
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     let client = Client::new(env::var("INFLUXDB_URL").unwrap(), "brewery");
 
-    let adapters = manager.adapters().unwrap();
-    let adapter = adapters
-        .into_iter()
-        .filter(|x| x.name == adapter_name)
-        .nth(0)
-        .unwrap();
+    let manager = Manager::new().await?;
+    let adapters = manager.adapters().await?;
 
-    let central = adapter.connect().unwrap();
+    let mut streams = Vec::new();
 
-    let event_receiver = central.event_receiver().unwrap();
+    for adapter in adapters.iter() {
+        let events = adapter.events().await?;
+        adapter.start_scan(ScanFilter::default()).await?;
+        streams.push(events);
+    }
 
-    central.start_scan().unwrap();
+    let mut events = select_all(streams);
 
-    while let Ok(event) = event_receiver.recv() {
+    while let Some(event) = events.next().await {
         match event {
-            CentralEvent::DeviceUpdated(bd_addr) => {
-                let peripheral = central.peripheral(bd_addr).unwrap();
-                match peripheral.properties().manufacturer_data {
-                    Some(data) => {
-                        let len = data.len();
-                        if len >= 6 && &data[0..6] == TILT_MFG_ID {
-                            let color = code_to_color(data[7]).unwrap();
-                            let temp = ((data[len - 5] as u16) << 8) | (data[len - 4] as u16);
-                            let sg_int = ((data[len - 3] as u16) << 8) | (data[len - 2] as u16);
-                            let sg = (sg_int as f32) / 1000.0;
+            CentralEvent::ManufacturerDataAdvertisement {
+                manufacturer_data, ..
+            } => {
+                if let Some(data) = manufacturer_data.get(&TILT_MFG_ID) {
+                    let len = data.len();
+                    let color = code_to_color(data[5]).unwrap();
+                    let temp = ((data[len - 5] as u16) << 8) | (data[len - 4] as u16);
+                    let sg_int = ((data[len - 3] as u16) << 8) | (data[len - 2] as u16);
+                    let sg = (sg_int as f32) / 1000.0;
 
-                            let reading = TiltReading {
-                                time: Utc::now(),
-                                color: color_name(color),
-                                temp: temp,
-                                sg: sg,
-                            };
+                    let reading = TiltReading {
+                        time: Utc::now(),
+                        color: color_name(color),
+                        temp: temp,
+                        sg: sg,
+                    };
 
-                            println!("tilt: {:?}", reading);
+                    println!("tilt: {:?}", reading);
 
-                            let _ = client.query(&reading.into_query("tilt")).await;
-                        }
-                    }
-                    None => {}
+                    let _ = client.query(&reading.into_query("tilt")).await;
                 }
             }
             _ => {}
         }
     }
+
+    Ok(())
+
+    // let adapter_name = env::var("BT_ADAPTER").unwrap();
+
+    // let client = Client::new(env::var("INFLUXDB_URL").unwrap(), "brewery");
+
+    // let adapter = adapters
+    //     .into_iter()
+    //     .filter(|x| x.name == adapter_name)
+    //     .nth(0)
+    //     .unwrap();
+
+    // let central = adapter.connect().unwrap();
+
+    // let event_receiver = central.event_receiver().unwrap();
+
+    // central.start_scan().unwrap();
+
+    // while let Ok(event) = event_receiver.recv() {
+    //     match event {
+    //         CentralEvent::DeviceUpdated(bd_addr) => {
+    //             let peripheral = central.peripheral(bd_addr).unwrap();
+    //             match peripheral.properties().manufacturer_data {
+    //                 Some(data) => {
+    //                     let len = data.len();
+    //                     if len >= 6 && &data[0..6] == TILT_MFG_ID {
+    //                         let color = code_to_color(data[7]).unwrap();
+    //                         let temp = ((data[len - 5] as u16) << 8) | (data[len - 4] as u16);
+    //                         let sg_int = ((data[len - 3] as u16) << 8) | (data[len - 2] as u16);
+    //                         let sg = (sg_int as f32) / 1000.0;
+
+    //                         let reading = TiltReading {
+    //                             time: Utc::now(),
+    //                             color: color_name(color),
+    //                             temp: temp,
+    //                             sg: sg,
+    //                         };
+
+    //                         println!("tilt: {:?}", reading);
+
+    //                         let _ = client.query(&reading.into_query("tilt")).await;
+    //                     }
+    //                 }
+    //                 None => {}
+    //             }
+    //         }
+    //         _ => {}
+    //     }
+    // }
 }
